647d56b2a7a0bada7ce14b897bcf734a72f8050a
Hermínio Alves <hermy.ay@gmail.com>
2021-10-13 22:25:07 +0200

    update

--- interompendo_repeticoes_while/ex71.py
+++ interompendo_repeticoes_while/ex71.py
@@ -1,8 +1,16 @@
 # Crie um programa que simule o funcionamento de um caixa electronico. No inicio pergunte ao usuario qual sera' o valor a ser sacado (numero inteiro) e o programa vai informar quantas cedulas de cada valor serao entregues.
 # OBS: Considere que o caixa possui cedulas de MZN50, MZN20, MZN10 e MZN1.
 
-import random
+from random import randint
 while True:
     pergunta = int(input("Qual e' o valor a ser levantado: "))
-    random.randint(50,20,10,1)
-    print(f"levantou {pergunta} notas")
+    cedula1 = 1
+    cedula2 = 10
+    cedula3 = 20
+    cedula4 = 50
+    totalcedulas = cedula1 + cedula2 + cedula3 +cedula4
+    while totalcedulas == pergunta:
+        randint(totalcedulas)
+        print 
+    
+
