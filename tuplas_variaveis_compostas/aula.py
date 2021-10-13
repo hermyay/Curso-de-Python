2412f91adc98bf21c26687c594d41a965fef3c6c
Hermínio Alves <hermy.ay@gmail.com>
2021-10-13 22:45:45 +0200

    tuplas

---
+++ tuplas_variaveis_compostas/aula.py
@@ -0,0 +1,5 @@
+'''
+Tupla e' uma variavel que armazena varios valores.
+Os elementos da tupla, ou seja, os valores, sao identificados por indices que inicia, como nas listas, de 0.
+Print de um valor em especifico da dupla: print(variavel [2]) 
+'''
