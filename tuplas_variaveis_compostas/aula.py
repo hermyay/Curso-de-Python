343d0af8ff27fb14a48adce8ab7d02cd81d6cac0
Hermínio Alves <hermy.ay@gmail.com>
2021-10-29 15:13:31 +0200

    update

--- tuplas_variaveis_compostas/aula.py
+++ tuplas_variaveis_compostas/aula.py
@@ -1,5 +1,10 @@
 '''
-Tupla e' uma variavel que armazena varios valores.
-Os elementos da tupla, ou seja, os valores, sao identificados por indices que inicia, como nas listas, de 0.
-Print de um valor em especifico da dupla: print(variavel [2]) 
+Tupla: E' uma variavel que armazena varios valores. Ou seja, Tupla e' uma variavel composta.
+Os elementos da tupla, ou seja, os valores, sao identificados por indices numericos que iniciam, como nas listas, de 0 e os elementos das tuplas sao acessados atraves desses indices numericos.
+Se, por exemplo, numa Tupla de variavel "lanche" e com elementos "amburguer, sumo, piza, pudim", se quiser mostrar o terceiro elemento dessa Tupla, o comando a ser digitado sera: print(lanche[2]) e o valor da saida sera' piza.
+Se, por exemplo, na Tupla de variavel "lanche", quiser mostrar na saida os valores "amburguer, sumo". O comando a ser executado sera' print(lanche[0,2]). NB: Note-se que, o ultimo valor "piza" que coresponde ao indice [2] e' sempre ignorado.
+NB: As Tuplas sao imutaveis. Uma vez defenida dentro do programa ela se torna imutavel. 
+Existem tres tipos de variaveis compostas: a primeira, sao as tuplas, a segunda sao as listas e a terceira sao os dicionarios.
 '''
+# Exemplo de uma tupla:
+lanche = (amburguer, 
