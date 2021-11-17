c741f3e2e208aa0d3d1c8812dae7055758d083b8
Hermínio Alves <hermy.ay@gmail.com>
2021-11-17 21:10:54 +0200

    update

--- tuplas_variaveis_compostas/ex072.py
+++ tuplas_variaveis_compostas/ex072.py
@@ -1,6 +1,20 @@
 # Crie um programa que tenha uma tupla totalmente preechida com uma contagem por extenso, de zero ate' vinte.
 # Seu programa devera' ler um numero pelo teclado (entre 0 e 20) e mostra'-lo por extenso.
 
-numero = "zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "catorze", "quinze", "desasseis", "dezassete", "dezoito", "dezanove", "vinte"
-questao = int(input("Digite um numero entre 0 e 20 e saiba o seu valor em extenso: "))
-print(f"Voce digitou o numero {numero[questao]}")
+valor = ("zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "catorze", "quinze", "desasseis", "dezassete", "dezoito", "dezanove", "vinte")
+
+while True:
+    print("="*50)
+    n = int(input("Digite um numero entre 0 e 20 e saiba o seu valor em extenso: "))
+    print("="*50)
+    if 0 <= n <= 20:
+        print(f"Voce digitou o numero {valor[n].title()}.")
+    else:
+        print("VALOR IRRECONHECIDO!\nPor favor, escolha um numero entre 0 e 20.")
+    print("="*50)
+    q = input("Deseja continuar?[S/N]: ").upper()
+    print("="*50)
+    if q in "nN":
+        break
+    elif q not in "SsnN":
+        print("Por favor, digite S para Sim e N para Nao: ").upper()
