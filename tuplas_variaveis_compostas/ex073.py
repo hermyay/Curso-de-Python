888e868817ed991c04c0aab4c5a92f961a4599f1
Hermínio Alves <hermy.ay@gmail.com>
2021-11-17 23:11:17 +0200

    update

---
+++ tuplas_variaveis_compostas/ex073.py
@@ -0,0 +1,12 @@
+# Crie uma tupla preenchida com os 20 primeiros colocados da tabela da Champions League, na ordem de colocacao. Depois mostre:
+# a) Apenas os 5 primeiros colocados.
+# b) Os ultimos 4 colocados da tabela.
+# c) Uma lista com as equipas em ordem alfabetica.
+# d) Em que posicao na tabela esta' a equipa do Real Madrid.
+
+chap = ("Real Madrid","Barcelona","Man. City","Chelsea","Dortmund","Juventos","Ajax","Atalanta","Atletico Madrid","Liverpool","Shakhtar Donetsk","Sevilla","Salzburg","Leipzig","Bayern","Benfica","Porto","Man. United","Paris","Villarreal")
+print(chap)
+print(chap[0:6])
+print(chap[16:20])
+print(sorted(chap))
+print(len("R"))
