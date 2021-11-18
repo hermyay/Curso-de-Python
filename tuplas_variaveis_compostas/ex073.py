f142fb1c0adc0e0c7095da557eacc0f1cba3d22f
Hermínio Alves <hermy.ay@gmail.com>
2021-11-18 07:42:04 +0200

    Update ex073.py
    NOTAS AOS for pos in chap[6] e for pos in range(len(chap[6]))
    NAO FAZEM PARTE DO EXERCICIO MAS SAO INTERRESANTES POR MOSTRAR OUTRAS FORMAS DE TRABALHAR AS TUPLAS.

--- tuplas_variaveis_compostas/ex073.py
+++ tuplas_variaveis_compostas/ex073.py
@@ -2,11 +2,23 @@
 # a) Apenas os 5 primeiros colocados.
 # b) Os ultimos 4 colocados da tabela.
 # c) Uma lista com as equipas em ordem alfabetica.
-# d) Em que posicao na tabela esta' a equipa do Real Madrid.
-
+# d) Em que posicao na tabela esta' a equipa do Ajax.
+print("TABELA DOS VINTE PRIMEIROS COLOCADOS DA CHAMPIONS")
 chap = ("Real Madrid","Barcelona","Man. City","Chelsea","Dortmund","Juventos","Ajax","Atalanta","Atletico Madrid","Liverpool","Shakhtar Donetsk","Sevilla","Salzburg","Leipzig","Bayern","Benfica","Porto","Man. United","Paris","Villarreal")
+print("=-="*20)
 print(chap)
-print(chap[0:6])
+print("=-="*20)
+print(chap[0:5])
+print("=-="*20)
 print(chap[16:20])
+print("=-="*20)
 print(sorted(chap))
-print(len("R"))
+print("=-="*20)
+for pos in range(len(chap[6])):
+    print(pos)
+print("=-="*20)
+for pos in chap[6]:
+    print(pos)
+print("=-="*20)
+for pos in enumerate(chap[6]):
+    print(f"O Ajax esta' na {pos} posicao.")
