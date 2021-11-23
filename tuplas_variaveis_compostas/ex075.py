4d515ab275125596b2d21bcd3b2cbec30889b814
Hermínio Alves <hermy.ay@gmail.com>
2021-11-23 19:44:38 +0200

    update

--- tuplas_variaveis_compostas/ex075.py
+++ tuplas_variaveis_compostas/ex075.py
@@ -1,6 +1,6 @@
 # Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
 # a) Quantas vezes apareceu o valor 9.
-# b) Em que posicao foi digitado o primeiro valor 3.
+# b) Em que posiço foi digitado o primeiro valor 3.
 # c) Quais foram os numeros pares.
 print("=-="*20)
 val1 = int(input("Digite o primeiro valor: "))
@@ -17,21 +17,16 @@ else:
     print(f"O numero 9 aparece quantas vezes?: NENHUMA VEZ.")
 print("=-="*20)
 if 3 in valor:
-    print(f"O primeiro numero 3 foi digitado em que posicao?: {valor.index(3)}a")
+    print(f"O primeiro numero 3 foi digitado em que posicao?: {valor.index(3)+1}ª")
 else:
     print("NAO EXISTE NUMERO 3 NESTE CONJUNTO.")
 print("=-="*20)
 print("Os valores pares sao: ",end="")
-if val1 % 2 == 0:
-    print(val1,end=", ")
-if val2 % 2 == 0:
-    print(val2,end=", ")
-if val3 % 2 == 0:
-    print(val3,end=", ")
-if val4 % 2 == 0:
-    print(val4)
-else:
-    print("Nao ha valores pares.")
+if val1 % 2 == 0: print(val1,end=", ")
+if val2 % 2 == 0: print(val2,end=", ")
+if val3 % 2 == 0: print(val3,end=", ")
+if val4 % 2 == 0: print(val4)
+else: print("Nao ha valores pares.") # Este metodo nao e' 100% eficaz.
 
 # OUTRA FORMA DE RESOLUCAO DO EXERCICIO
 
@@ -40,8 +35,26 @@ valor2 = tuple(int(input("Digite um numero: "))for v in range(0,4))
 print("=-="*20)
 print(f"O numero 9 aparece quantas vezes?: {valor2.count(9)}" if 9 in valor2 else "O numero 9 aparece quantas vezes?: NENHUMA.")
 print("=-="*20)
-print(f"O primeiro numero 3 foi digitado em que posicao?: {valor2.index(3)}a" if 3 in valor2 else "NAO EXISTE O NUMERO 3 NESTE CONJUNTO.")
+print(f"O primeiro numero 3 foi digitado em que posicao?: {valor2.index(3)+1}ª" if 3 in valor2 else "NAO EXISTE O NUMERO 3 NESTE CONJUNTO.")
 print("=-="*20)
 print("Os valores pares sao: ",end=" ")
 print(n for n in valor2 if n % 2 == 0)
 print("=-="*20)
+
+# OUTRA MANEIRA DE RESOLVER O EXERCICIO
+print("=-="*20)
+valores3 = (int(input("Digite o primeiro valor: ")),int(input("Digite o segundo valor: ")),int(input("Digite o terceiro valor: ")),int(input("Digite o quarto valor: "))) # Lembrar que, nas ultimas versoes do python as tuplas dispensam parenteses.
+print("=-="*20)
+print(f"Voce digitou os seguintes valores: {valores3}")
+print("=-="*20)
+print(f"O nove aparece {valores3.count(9)} vezes")
+print("=-="*20)
+if 3 in valores3:
+    print(f"O numero 3 foi digitado na {valores3.index(3)+1}ª posicao.")
+else:
+    print("O valor 3 nao foi digitado em nenhuma posicao.")
+print("=-="*20)
+print("Os valores pares digitados foram", end=" ")
+for n in valores3:
+    if n % 2 == 0:
+        print(n, end=" ")
---
+++ tuplas_variaveis_compostas/ex076.py
@@ -0,0 +1,9 @@
+# Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços na sequêcia.
+# No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+
+print("---"*20)
+print("LISTAGEM DE PREÇOS")
+print("---"*20)
+precos = "Pão", 10, "Leite", 100, "Sumo", 85, "Yougurt", 35, "Jam", 200, "Cornofleks", 250, "Bolachas", 80, "Custard", 85, "Feijão", 100, "Manteiga", 55
+print(f"{precos[0]}..........{precos[1]} MZN$")
+print("---"*20)
