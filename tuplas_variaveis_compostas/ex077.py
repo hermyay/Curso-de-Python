12a5486a30b5e41e4645d69fcf1f853584e89b12
Hermínio Alves <hermy.ay@gmail.com>
2021-11-25 13:57:54 +0200

    Update ex077.py

--- tuplas_variaveis_compostas/ex077.py
+++ tuplas_variaveis_compostas/ex077.py
@@ -1,3 +1,14 @@
 # Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
-palavra = "ambiguo", "literatura", "arte", "constragimento", "accao", "paixao", "severidade", "oito"
+print("=="*30)
+print(f"{'DEMONSTRAÇÃO DE VOGAIS':^60}")
+print("=="*30)
+palavra = "ambíguo", "literatura", "arte", "constragimento", "acção", "paixão", "severidade", "oito"
+for pal in palavra:
+    print(f"\nNa palavra {pal.title()}, existem as vogais: ",end="")
+    for cada in pal:
+        if cada in "aãeiíou":
+            print(cada,end=" ")
+            
+    print("\n")
+    print("=="*30)
