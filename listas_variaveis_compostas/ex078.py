21881eadc6966f3cccb9b16efe7946a6e92c4786
Hermínio Alves <hermy.ay@gmail.com>
2021-12-15 12:53:46 +0200

    update

--- listas_variaveis_compostas/ex078.py
+++ listas_variaveis_compostas/ex078.py
@@ -1,30 +1,37 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 
+# Apresentacao do Jogo.
 print("=-="*10)
 print(f"{'O JOGO DO MAIOR E MENOR':^31}")
 print("=-="*10)
 
+# Criacao da Lista a receber valores.
 valores = list()
 for c in range(0, 5):
     valores.append(int(input(f"Digite um número para a posicao {c}: ")))
 
-
+# Apresentacao dos valores e criacao de variaveis para o maior e o menor valor.
 print("=-="*10)
 print(f"Digitou os seguintes valores: {valores}")
 
 maior = max(valores)
 menor = min(valores)
 
+# Apresentacao do maior valor digitado.
 print("=-="*10)
 print(f"O MAIOR valor digitado foi {max(valores)} e se encontra nas posiçoes",end=" ")
 
-# Se o valor digitado for igual a maior, ou seja, `a variavel que recebe o maior valor digitado, imprima a posicao desse valor, ou, posicoes desse valor.
+# Para apresentacao das posicoes do maior valor digitado, no caso de o mesmo valor ser digitado duas ou mais vezes.
+# Se o valor digitado for igual `a variavel maior, ou seja, `a variavel que recebe o maior valor digitado, o programa imprime a posicao desse valor, ou, em caso do valor ser apresentado em duas ou mais posicoes, imprime as posicoes desse valor.
 for i, v in enumerate(valores):
     if v == maior:
         print(f"{i}...",end=" ")
 print()
 
+# Apresentacao do menor valor digitado.
 print(f"O MENOR valor digitado foi {min(valores)} e se encontra nas posiçoes",end=" ")
+
+# Apresentacao das posicoes dos menores valores digitados, seguindo a logica de programacao para o maior valor digitado.
 for i, v in enumerate(valores):
     if v == menor:
         print(f"{i}...",end="")
--- listas_variaveis_compostas/ex079.py
+++ listas_variaveis_compostas/ex079.py
@@ -1,18 +1,34 @@
 # Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 
+# Apresentacao do Programa.
+print("=-="*20)
+print(f"{'FICHA DE CADASTRO DE NUMEROS SEM REPETICAO DE VALORES':^62}")
+print("=-="*20)
+
+# Criacao da Lista de recebimento de cadastros e da declaracao de valores ja adicionados.
 valores = []
 while True:
-    n = int(input("Digite um valor: "))
+    n = int(input("Digite o valor: "))
     if n not in valores:
         valores.append(n)
     else:
         print("O valor já foi adicionado.")
+    
+
+# Criacao da pergunta para parar, ou nao, recebimento de novos valores.
     perg = input(str("Quer continuar? [S/N]: ")).strip().upper()[0]
+
+# Direccionamento `as opcoes de reposta `a pergunta sobre recebimento de novos valores.
     while perg not in "SsNn":
         perg = input(str("Por favor, digite Sim ou Nao.\nQuer continuar? [S/N]: ")).strip().upper()[0]
+
+# Paragem para o recebimento de novos valores. Break.
     if perg in "Nn":
         print("=-="*20)
         valores.sort()
         print(f"Os numeros escolhidos foram: {valores}")
         break
-print("GAME OVER!")
+print("=-="*7)
+print("CADASTRO TERMINADO!")
+
+# NB: SEMPRE TER ATENCAO `A INDENTACAO, E' FULCAR PARA O ANDAMENTO CORECTO DO PROGRAMA.
